88da5a22850e2170d51e1450b2790c6d9d6a1cfb
Pedro Ângelo Peregrino e Silva <140512570+Fourx87@users.noreply.github.com>
2023-11-19 20:31:06 -0300

    add aula_declaracaoVaiaveis, aula_manipulandoMatrizesVetores

---
+++ aula_declarandoVariaveis/src/script.js
@@ -0,0 +1,15 @@
+//Saida
+console.log("Digite o nome do seu jogador: ");
+
+//Declarando variável
+let nickname = "Pedro";
+
+//Concatenação -> mensagem fixa + variável
+console.log("Bem vindo " + nickname);
+console.log(nickname + " entrou no servidor.")
+
+//Declaração de constante
+const notificacao = "Lembre-se sempre de ";
+
+console.log(notificacao + "de escovar o dente antes de dormir.")
+console.log(notificacao + "lavar as mãos antes de comer.")
---
+++ aula_manipulandoVetoresMatrizes/src/script.js
@@ -0,0 +1,13 @@
+//Vetores
+let pokemon = ["Pikachu", "Bulbassauro", "Chamander"];
+pokemon.shift(); //elimina o 1º elemento
+pokemon.pop(); //elimina o ultimo elemento.
+console.log(pokemon)
+
+//Matrizes
+let timePokemon = [
+    ["pikachu", "M", 55],
+    ["Chamander", "F", 85],
+    ["Bulbassauro", "M", 105]
+];
+console,log(timePokemon[0][2]);
